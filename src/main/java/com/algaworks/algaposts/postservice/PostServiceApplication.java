54feb55099c959ed7fa4e2bbeef550aa90af329b
Marco Aurelio Rodrigues Cavalheiro <marcorc30@gmail.com>
2025-05-02 20:37:06 -0300

    novos arquivos

---
+++ src/main/java/com/algaworks/algaposts/postservice/PostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaposts.postservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApplication.class, args);
+	}
+
+}
